58d591de41ea1790310c2c82bfb2ed6bf3260528
Stepan Kouba <>
2021-03-26 16:18:31 +0100

    fixed isVAT issue for all companies added tests to properly test values in isVAT

--- test/czares.spec.js
+++ test/czares.spec.js
@@ -42,10 +42,55 @@ describe('Ares SDK basic functions', () => {
             });
     });
 
-    it('Is VAT registered - freelancer', done => {
+    it('Get by Identification Number - freelancer VAT in past not now', done => {
+        const ares = new CZAres();
+
+        ares.getByIdentificationNumber('71700650')
+            .then(data => {
+                expect(data).to.have.property('idNumber');
+                expect(data.idNumber).equals('71700650');
+                expect(data.isVAT).to.be.false;
+                expect(data).to.have.property('taxIdNumber');
+                done();
+            }).catch(err => {
+                console.error(err);
+                done();
+            });
+    });
+
+    it('Get by Identification Number - compay VAT is valid now', done => {
+        const ares = new CZAres();
+
+        ares.getByIdentificationNumber('42192889')
+            .then(data => {
+                expect(data).to.have.property('idNumber');
+                expect(data.idNumber).equals('42192889');
+                expect(data.isVAT).to.be.true;
+                expect(data).to.have.property('taxIdNumber');
+                done();
+            }).catch(err => {
+                console.error(err);
+                done();
+            });
+    });
+
+    it('Is VAT registered - freelancer VAT in past not now', done => {
         const ares = new CZAres();
 
         ares.isVATRegistered('71700650')
+            .then(data => {
+                expect(data).to.be.false;
+                done();
+            }).catch(err => {
+                console.error(err);
+                done();
+            });
+    });
+
+    it('Is VAT registered - company VAT', done => {
+        const ares = new CZAres();
+
+        ares.isVATRegistered('42192889')
             .then(data => {
                 expect(data).to.be.true;
                 done();
